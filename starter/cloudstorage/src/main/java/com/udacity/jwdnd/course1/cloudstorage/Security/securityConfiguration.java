3609c04f2d18d0ffeb881ad063a4659a0946420e
dcsiona@gmail.com <dcsiona@gmail.com>
2021-03-05 05:42:25 -0500

    Updated package name.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/Security/securityConfiguration.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/Security/securityConfiguration.java
@@ -1,18 +1,14 @@
-package com.udacity.jwdnd.course1.cloudstorage.security;
+package com.udacity.jwdnd.course1.cloudstorage.Security;
 
 
 import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.core.Authentication;
 
-import java.util.ArrayList;
 
 @Configuration
 @EnableWebSecurity
