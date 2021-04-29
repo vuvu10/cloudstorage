056412a88d72bfccbbd8762e8718984659a42f36
dcsiona@gmail.com <dcsiona@gmail.com>
2021-04-29 18:20:54 -0400

    Updating FileMapper.java

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/Mapper/CredentialMapper.java
@@ -0,0 +1,29 @@
+package com.udacity.jwdnd.course1.cloudstorage.Mapper;
+
+
+import com.udacity.jwdnd.course1.cloudstorage.Model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.Model.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+    @Select("SELECT * FROM USERS WHERE credentialId = #{credentialId}")
+    Credential getCredential(Integer credentialId);
+
+    @Select("SELECT * FROM CREDENTIAL WHERE userId = #{userId}")
+    List<Credential> getAllCredential(Long userId);
+
+    @Insert("INSERT INTO CREDENTIAL (url, username, key, password, userId) VALUES (#{url}, #{username}, #{key}, #{password}, #{userId})" )
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+   public Integer insert(User user);
+
+    @Update("UPDATE CREDENTIAL SET url = #{url}, username = #{username}, key = #{key}, password = #{password}, userid = #{userid} WHERE credentialId = #{credentialId}")
+    public int updateCredential(Credential credential);
+
+    @Delete("DELETE FROM CREDENTIAL WHERE credentialId = #{ credentialId}")
+    public int deleteCredential(int credentialId);
+
+
+}
